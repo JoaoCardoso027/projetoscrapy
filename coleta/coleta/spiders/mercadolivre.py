597a96527dffc114cba4a2e743c963f3613b728f
Bowie <jpneto06@outlook.com>
2024-10-16 10:17:47 -0300

    ajustes no mercadolivre.py

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+
+    def parse(self, response):
+        products = response.css('div.ui-search-result__content')
+        
+        for product in products:
+            
+            yield {
+                'brand': product.css('span.ui-search-item__brand-discoverability.ui-search-item__group__element::text').get(),
+                'name': product.css('class.ui-search-item__title.ui-search-item__group__element::text').get() # erro aqui
+            }
