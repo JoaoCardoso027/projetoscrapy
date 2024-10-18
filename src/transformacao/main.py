334617a2452a5d3d5d7cf00d25244bb34f84a4a9
Bowie <jpneto06@outlook.com>
2024-10-17 22:35:28 -0300

    feat: adicionar transformacao com pandas

--- src/transformacao/main.py
+++ src/transformacao/main.py
@@ -11,5 +11,21 @@ df['_source'] = "https://lista.mercadolivre.com.br/tenis-corrida-masculino"
 df['_data_coleta'] = datetime.now()
 
 df['old_price_reais'] = df['old_price_reais'].fillna(0).astype(float)
+df['old_price_centavos'] = df['old_price_centavos'].fillna(0).astype(float)
+df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
+df['new_price_centavos'] = df['new_price_centavos'].fillna(0).astype(float)
+
+df['old_price'] = df['old_price_reais'] + df['old_price_centavos'] / 100
+df['new_price'] = df['new_price_reais'] + df['new_price_centavos'] / 100
+
+df.drop(columns=['old_price_reais', 'old_price_centavos', 'new_price_reais', 'new_price_centavos'], inplace=True)
+
+conn = sqlite3.connect('../data/quotes.db')
+
+df.to_sql('mercadolivre_items', conn, if_exists='replace', index=False)
+
+conn.close()
+
+print(df.head())
 
 
