a802c2c589594870bbbb8eff8dbc6d70e2528464
Bowie <jpneto06@outlook.com>
2024-10-17 23:17:05 -0300

    Corrigir new_price

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -3,30 +3,32 @@ import scrapy
 
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
     page_count = 1
     max_pages = 10
 
     def parse(self, response):
-        products = response.css('div.ui-search-result__content') # 54 itens
-
+        products = response.css('div.ui-search-result__content')
         
         for product in products:
-
+            
             prices = product.css('span.andes-money-amount__fraction::text').getall()
             cents = product.css('span.andes-money-amount__cents::text').getall()
-
+            
             yield {
                 'brand': product.css('span.ui-search-item__brand-discoverability.ui-search-item__group__element::text').get(),
-                'name': product.css('h2.ui-search-item__title::text').get(),
+                'name': product.css('h2.ui-search-item__title a::text').get(),
                 'old_price_reais': prices[0] if len(prices) > 0 else None,
                 'old_price_centavos': cents[0] if len(cents) > 0 else None,
                 'new_price_reais': prices[1] if len(prices) > 1 else None,
                 'new_price_centavos': cents[1] if len(cents) > 1 else None,
             }
-
+            
         if self.page_count < self.max_pages:
             next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
             if next_page:
                 self.page_count += 1
-                yield scrapy.Request(url=next_page, callback=self.parse)
+                next_page_url = response.urljoin(next_page)  # Resolve o link relativo para absoluto
+                yield scrapy.Request(url=next_page_url, callback=self.parse)
+            
